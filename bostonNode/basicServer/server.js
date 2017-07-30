0e418f669b3f296da6d8756fe6b47ab6b441e327
rmanese <rjmanese@gmail.com>
2017-07-29 20:58:54 -0700

    starting to get a better grasp of node, hosted a server and sent html file through. About to start enter basic express

--- bostonNode/basicServer/server.js
+++ bostonNode/basicServer/server.js
@@ -1,11 +1,21 @@
+var fs = require('fs');
 var http = require('http');
 
-function onRequest(request, response) {
-    console.log("A user made a request" + request.url);
-    response.writeHead(200, {"Context-Type": "text/plain"});
-    response.write("Here is some date");
+function send404Response(response) {
+    response.writeHead(404, {"Content-Type" : "text/plain"});
+    response.write("Error 404: Page not found!");
     response.end();
 }
 
+function onRequest(request, response) {
+
+    //if the method is get and the request url is homepage
+    if ( request.method == 'GET' && request.url == '/') {
+        //ask for an html file, read it and send it back as response
+        response.writeHead(200, {"Content-Type" : "text/html"});
+        fs.createReadStream("./index.html").pipe(response);
+    }
+}
+
 http.createServer(onRequest).listen(8888);
-console.log("Server is now running...");
+console.log("Server is running... ");
